6935b09587c0587039c57628d16512ff1e821d33
bmmasaputra <bimagung2203@gmail.com>
2024-12-02 16:07:08 +0700

    first push to remote

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: true
+}));
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send("<h1 style='font-family: sans-serif; margin: 50px; font-weight: 400'>Hi user! Welcome to VM Workflow</h1>");
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# GCP VM WORKFLOW
+
+### Hello there! 
+
+#### This repo is a demonstration for implementing CI/CD using github action to Google Cloud Platform VM Instance
